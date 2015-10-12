fba1050018ba24dacf8d5bc5faf35197e51c861e
nirav <niravsheth21@gmail.com>
2015-10-11 20:41:50 -0400

    Added a few features. 10/11/2015

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function save() {
 		var listData = document.getElementById('todoList').innerHTML;
 		localStorage.setItem('list', listData);
-		window.alert("List Saved.");
+		alert("List Saved.");
 }
 
 function remove() {
@@ -16,14 +16,58 @@ function load() {
 		}
 }
 
-
-//<li>Test <button type="button" name="delete" onclick="delete()" ><img src="delete.png" /></button></li>
-function newItem() {
-	var inp = document.getElementById('newItem');
-	if (inp.value != "") {
-		var ul = document.getElementById("todoList");
-		var li = '<li><button type="button" name="delete" id="" + 'onclick="delete()" ><img src="delete.png" /></button>' + inp.value + '</li>';
-		ul.innerHTML += li;
-		inp.value = "";
-	}
+function updateItemStatus() {
+    var chkId = this.id.replace("chk_", "");
+    var span = document.getElementById("itm_" + chkId);
+    if(this.checked) {
+        span.className = "checked";
+    } else {
+        span.className = "";
+    }
 }
+
+function delItem() {
+    var btnId = this.id.replace("del_", "");
+    var li = document.getElementById("li_" + btnId);
+    var ul = li.parentElement;
+    ul.removeChild(li);
+    localStorage.setItem('list', ul.innerHTML);
+    alert("Item deleted, List saved.");
+}
+
+function newItem(ul, itmText) {
+	if (itmText.trim() != "") {
+        totalItems++;
+		var li = document.createElement("li");
+        li.id = "li_" + totalItems;
+        var chk = document.createElement("input");
+        chk.type = "checkbox";
+        chk.id = "chk_" + totalItems;
+        chk.onclick = updateItemStatus;
+        var span = document.createElement("span");
+        span.id = "itm_" + totalItems;
+        span.innerText = itmText;
+
+        var del = document.createElement("button");
+        del.id = "del_" + totalItems;
+        del.onclick = delItem;
+
+        var img = document.createElement("img");
+        img.text = "Delete";
+        img.src = "delete.png";
+        del.appendChild(img);
+
+        li.appendChild(del);
+        li.appendChild(chk);
+        li.appendChild(span);
+		ul.appendChild(li);
+	} else { alert('Please provide item.');}
+}
+
+var totalItems = 0;
+var btnNewItem = document.getElementById('addNewItem');
+btnNewItem.onclick = function() {
+    var inp = document.getElementById('newItem');
+    newItem(document.getElementById('todoList'), inp.value);
+    inp.value = "";
+};
